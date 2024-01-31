7c7189e2fc12eecd035ce220d8c3a56629708e54
Arthur Martins <143364331+ArthurMartinns@users.noreply.github.com>
2024-01-31 09:51:08 -0300

    Add files via upload

---
+++ Js/script.js
@@ -0,0 +1,62 @@
+// Questão 01
+alert("Hello, word!")
+
+// Questão 2
+let number1 = 3;
+let number2 = 2;
+
+alert(number1 + number2)
+
+// Questão 3
+let numberTeste = 1;
+if (typeof numberTeste == "number") {
+    alert("Sim, é um número")
+}
+else {
+    alert("Não é")
+}
+
+// Questão 4
+
+let txtTeste = "a"
+if (typeof txtTeste == "string") {
+    alert("Sim, é uma string")
+}
+else {
+    alert("Não é uma string")
+}
+
+// Questão 5
+
+let boolTeste = true
+if (typeof boolTeste == "boolean") {
+    alert("Sim, é uma boolean")
+}
+else {
+    alert("Não é uma boolean")
+}
+
+// Questão 6, 7 e 8
+
+let n3 = 5
+let n4 = 10
+
+const sub = n3 - n4
+alert("A subtração é:" + sub)
+
+const mult = n3 * n4
+alert("A Multiplicação é:" + mult)
+
+const div = n3 / n4
+alert("A Divisão é:" + sub)
+
+
+// Questão 9 e 10
+
+let number = 1
+if (number % 2 == 0) {
+    alert("É um número par")
+}
+else {
+    alert("É um número impar")
+}
